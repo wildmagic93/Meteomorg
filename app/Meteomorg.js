3c1832670c4385db477322f0732d15c44faa8ad4
wildmagic93 <paologalletta93@gmail.com>
2021-07-05 16:27:38 +0200

    First commit

---
+++ app/Meteomorg.js
@@ -0,0 +1,17 @@
+const sentenze = ['a chissu fra tri ghionna mori', 'Attento alla DELTAAAAAAAA', 'viri ca ci su petri na lenticchiaaaaa', "te l'hau rittu iu!", 'a rilla quacchi cosa!', 'a mettiti sa spacchiu ri mascherina!'];
+
+
+const bottone = document.getElementById('bottone');
+const testo = document.getElementById('frase');
+//testo.innerHTML = 'aaaa';
+
+function sent()
+{
+	testo.innerHTML = '';
+	const nome = document.getElementById('input').textContent;
+	const frase = 'AH-HA, ' + sentenze[Math.floor(Math.random() * sentenze.length)];
+	testo.innerHTML = frase;
+	
+}
+
+bottone.addEventListener('click', sent);
