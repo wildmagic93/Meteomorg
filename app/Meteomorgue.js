ac1a361d874ecd4dd32c64a7fe77c7c4e42cb73c
wildmagic93 <paologalletta93@gmail.com>
2021-07-11 17:46:03 +0200

    commit

--- app/Meteomorgue.js
+++ app/Meteomorgue.js
@@ -1,4 +1,4 @@
-const sentenze = ['a tiaaaaaa, ombrellinoooo!', 'Vu viriri ca fazzu u fitusu?', 'a fattilla na passiata!', 'a tantu nun ci ne coviddi!', 'chistu ie cagnuleddu', 'a chissu fra tri ghionna mori', 'Attento alla DELTAAAAAAAA', 'viri ca ci su petri na lenticchiaaaaa', "te l'hau rittu iu!", 'a rilla quacchi cosa!', 'a mettiti sa spacchiu ri mascherina!'];
+const sentenze = ['a tiaaaaaa, ombrellinoooo!', 'vu viriri ca fazzu u fitusu?', 'a fattilla na passiata!', 'a tantu nun ci ne coviddi!', 'chistu ie cagnuleddu', 'a chissu fra tri ghionna mori', 'Attento alla DELTAAAAAAAA', 'viri ca ci su petri na lenticchiaaaaa', "te l'hau rittu iu!", 'a rilla quacchi cosa!', 'a mettiti sa spacchiu ri mascherina!'];
 
 
 const bottone = document.getElementById('bottone');
